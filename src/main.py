bdcb32dd6337c9793ac48ec48f5857d44cf5c107
1harz <raulfalluh@gmail.com>
2026-06-09 10:27:45 -0300

    feat(api): criacao do endpoint de cadastro de novos usuarios (POST /users)

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from src.models import UserCreate
 
 app = FastAPI(title='Automotive Recommendation API')
 
@@ -10,3 +11,7 @@ def read_root():
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
+
+@app.post("/users")
+def create_user(user: UserCreate):
+    return user
