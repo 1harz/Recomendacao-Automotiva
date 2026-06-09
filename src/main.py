f2a356a06f677aa950629eaadc6cbb202443157c
1harz <raulfalluh@gmail.com>
2026-06-09 10:22:38 -0300

    feat(api): implementacao da rota de health check para monitoramento do estado do sistema

--- src/main.py
+++ src/main.py
@@ -6,3 +6,7 @@ app = FastAPI(title='Automotive Recommendation API')
 @app.get("/")
 def read_root():
     return {"message": "Welcome to Automotive Recommendation API", "version": "1.0.0"}
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
